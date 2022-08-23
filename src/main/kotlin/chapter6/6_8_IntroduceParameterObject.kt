1499869d0259fe2bc2527186f75d3ec33c3975d9
Jongho <tghbjhpark@gmail.com>
2022-08-23 23:38:02 +0900

    [6.8](1)기본예제

---
+++ src/main/kotlin/chapter6/6_8_IntroduceParameterObject.kt
@@ -0,0 +1,39 @@
+package chapter6
+
+fun readingsOutsideRange(station: Station, min: Int, max: Int): List<Reading> {
+    return station.readings.filter {
+        it.temp < min || it.temp > max
+    }
+}
+
+data class Station(
+    val name: String,
+    val readings: List<Reading>
+)
+
+data class Reading(
+    val temp: Int,
+    val time: String
+)
+
+// Caller
+data class OperatingPlan(
+    val temperatureFloor: Int,
+    val temperatureCeiling: Int
+)
+
+fun main() {
+    val operatingPlan = OperatingPlan(24, 27)
+    val alerts = readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling)
+}
+
+val station = Station(
+    name = "ZB1",
+    readings = listOf(
+        Reading(47, "2016-11-10 09:10"),
+        Reading(53, "2016-11-10 09:20"),
+        Reading(58, "2016-11-10 09:30"),
+        Reading(53, "2016-11-10 09:40"),
+        Reading(51, "2016-11-10 09:50")
+    )
+)
