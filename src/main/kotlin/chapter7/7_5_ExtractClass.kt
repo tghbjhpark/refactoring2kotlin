ecc2538dad6018cb2cdf454ca7971ff14e9da47b
Jongho <tghbjhpark@gmail.com>
2022-08-23 12:21:17 +0900

    7.5 Extract Class 3) Person 클래스의 인스턴스 생성시 telephoneNumber 인스턴스도 함께 생성

--- src/main/kotlin/chapter7/7_5_ExtractClass.kt
+++ src/main/kotlin/chapter7/7_5_ExtractClass.kt
@@ -3,9 +3,10 @@ package chapter7
 class Person(
     val name: String,
     val officeAreaCode: String,
-    val officeNumber: String
+    val officeNumber: String,
+    val telephoneNumber: TelephoneNumber
 ) {
     fun getTelephoneNumber() = "($officeAreaCode) $officeNumber"
 }
 
-class TelephoneNumber
+class TelephoneNumber()
