24ddb5b5c4e7e0eb7dd845de6a2995fe0e608257
Jongho <tghbjhpark@gmail.com>
2022-08-23 12:31:36 +0900

    7.5 Extract Class 5-3) 정리 - 변경되어 사용하지 않는 필드 삭제

--- src/main/kotlin/chapter7/7_5_ExtractClass.kt
+++ src/main/kotlin/chapter7/7_5_ExtractClass.kt
@@ -2,8 +2,6 @@ package chapter7
 
 class Person(
     val name: String,
-    val officeAreaCode: String,
-    val officeNumber: String,
     val telephoneNumber: TelephoneNumber
 ) {
     fun getTelephoneNumber() = telephoneNumber.toString()
@@ -13,8 +11,6 @@ class TelephoneNumber(
     val areaCode: String,
     val number: String
 ) {
-    fun getTelephoneNumber() = "($areaCode) $number"
-
     override fun toString(): String {
         return "($areaCode) $number"
     }
