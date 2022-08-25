5f745254c9d0c069583d441833dd77ddb8401ef5
Jongho <tghbjhpark@gmail.com>
2022-08-25 22:08:32 +0900

    [10.4](3)switch 문의 절 하나를 서브클래스에서 오버라이드 하기

--- src/main/kotlin/chapter10/10_4_ReplaceConditionalWithPolymorphism.kt
+++ src/main/kotlin/chapter10/10_4_ReplaceConditionalWithPolymorphism.kt
@@ -9,13 +9,7 @@ sealed class Bird(
 ) {
     open fun plumage(): String = "알 수 없다"
 
-    fun airSpeedVelocity(): Int? =
-        when (type) {
-            "유럽 제비" -> 35
-            "아프리칸 제비" -> 40 - 2 * numOfCoconuts
-            "노르웨이 파랑 앵무" -> if (isNailed) 0 else 10 + voltage / 10
-            else -> null
-        }
+    open fun airSpeedVelocity(): Int? = null
 
     fun createBird() =
         when (type) {
@@ -29,18 +23,24 @@ sealed class Bird(
         bird.type, bird.name, bird.numOfCoconuts, bird.voltage, bird.isNailed
     ) {
         override fun plumage(): String = "보통이다"
+
+        override fun airSpeedVelocity(): Int? = 35
     }
 
     class AfricanSwallow(bird: Bird): Bird(
         bird.type, bird.name, bird.numOfCoconuts, bird.voltage, bird.isNailed
     ) {
         override fun plumage(): String = if (numOfCoconuts > 2) "지쳤다" else "보통이다"
+
+        override fun airSpeedVelocity(): Int? = 40 - 2 * numOfCoconuts
     }
 
     class NorwegianBlueParrot(bird: Bird): Bird(
         bird.type, bird.name, bird.numOfCoconuts, bird.voltage, bird.isNailed
     ) {
         override fun plumage(): String = if (voltage > 100) "그을렸다" else "예쁘다"
+
+        override fun airSpeedVelocity(): Int? = if (isNailed) 0 else 10 + voltage / 10
     }
 }
 
